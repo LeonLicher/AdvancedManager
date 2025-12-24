04ba9af0e263115da44db255b444535f1f37aee9
Leon Licher <licher.leon@gmail.com>
2025-12-24 16:25:32 +0100

    save

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern-compiler",
-        quietDeps: true,
-      }
-    }
-  },
-  plugins: [react()],
-  base: "/AdvancedManager/",
-  optimizeDeps: {
-    include: ["firebase/app", "firebase/firestore"],
-  },
-});
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler',
+                quietDeps: true,
+            },
+        },
+    },
+    plugins: [react()],
+    base: '/AdvancedManager/',
+    optimizeDeps: {
+        include: ['firebase/app', 'firebase/firestore'],
+    },
+})
